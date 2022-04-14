830d9d05b3534486eb7eb7bf0cde27f8145bc45e
NoxDecima <sven@8x10.de>
2022-04-14 13:05:55 +0200

    First attempts for API connection
    (cherry picked from commit 397ab8b41c50ce4d4f518d82867faba19576a4ca)

---
+++ Assets/CustomAssets/Scripts/API/APIServerConfig.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIPServerConfig
+{
+
+    // URL with place to put API method in it.
+    public const string SERVER_API_URL_FORMAT = "http://http://www.leerpaden.nl/";
+    // Mocky generates random strings for your endpoints, you should name them properly!
+    public const string API_GET_LIVE = SERVER_API_URL_FORMAT + "get_instances/user={0}";
+    public const string API_GET_INSTANCE = SERVER_API_URL_FORMAT + "live/user={0}";
+
+}
