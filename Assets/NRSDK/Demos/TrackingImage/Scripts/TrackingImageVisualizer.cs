33504ffc885f639b39e9cbb8e103a97b45d44ad1
rroell <roelliie@hotmail.nl>
2022-04-20 15:28:10 +0200

    Got QR code working
    Got is working. Now a white box is shown when the QR code is in vision

--- Assets/NRSDK/Demos/TrackingImage/Scripts/TrackingImageVisualizer.cs
+++ Assets/NRSDK/Demos/TrackingImage/Scripts/TrackingImageVisualizer.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using NRKernal;
+using System;
 
 public class TrackingImageVisualizer : MonoBehaviour
 {
@@ -12,11 +13,13 @@ public class TrackingImageVisualizer : MonoBehaviour
 
     void Update()
     {
+        Console.WriteLine("UPDATE! function called");
         if(image == null)
         {
             cube.SetActive(false);
             return;
         }
+        Console.WriteLine("Image detected!");
         var center = image.GetCenterPose();
         transform.position = center.position;
         transform.rotation = center.rotation;
