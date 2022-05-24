2ef3cfa1f96826dccb7a068121ee5797574bd374
NoxDecima <sven@8x10.de>
2022-05-24 11:22:28 +0200

    Properly connect image tracking

--- Assets/CustomAssets/Scripts/Mode/DisplayMode.cs
+++ Assets/CustomAssets/Scripts/Mode/DisplayMode.cs
@@ -2,50 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DisplayMode : MonoBehaviour
-{
-    public enum Mode
+namespace NRKernal{
+    public class DisplayMode : MonoBehaviour
     {
-        NAME, TOPIC, PROGRESS, MOOD
-    }
-    
-    private Mode _displayMode;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        this._displayMode = Mode.NAME;
-    }
-
-    public void setMode(Mode mode)
-    {
-        this._displayMode = mode;
-    }
-
-    public Mode getMode()
-    {
-        return _displayMode;
-    }
-
-    public void cycleMode() 
+        public enum Mode
         {
-            switch(_displayMode)
+            NAME, TOPIC, PROGRESS, MOOD
+        }
+        
+        private Mode _displayMode;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            this._displayMode = Mode.NAME;
+        }
+
+        void Update()
+        {
+            if(NRInput.GetButtonDown(ControllerButton.APP))
             {
-                case Mode.NAME: 
-                    setMode(Mode.TOPIC);
-                    break;
-                
-                case Mode.TOPIC: 
-                    setMode(Mode.PROGRESS);
-                    break;
-                
-                case Mode.PROGRESS: 
-                    setMode(Mode.MOOD);
-                    break;
-                
-                case Mode.MOOD: 
-                    setMode(Mode.NAME);
-                    break;
+                cycleMode();
             }
         }
+
+        public void setMode(Mode mode)
+        {
+            this._displayMode = mode;
+        }
+
+        public Mode getMode()
+        {
+            return _displayMode;
+        }
+
+        public void cycleMode() 
+            {
+                switch(_displayMode)
+                {
+                    case Mode.NAME: 
+                        setMode(Mode.TOPIC);
+                        break;
+                    
+                    case Mode.TOPIC: 
+                        setMode(Mode.PROGRESS);
+                        break;
+                    
+                    case Mode.PROGRESS: 
+                        setMode(Mode.MOOD);
+                        break;
+                    
+                    case Mode.MOOD: 
+                        setMode(Mode.NAME);
+                        break;
+                }
+            }
+    }
 }
---
+++ Assets/CustomAssets/Scripts/StudentVisualizer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NRKernal 
+{
+    public class StudentVisualizer : MonoBehaviour
+    {
+        public int StudentID;
+
+        public DisplayMode DisplayMode;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
